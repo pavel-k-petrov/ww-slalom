1baa8f5784a0c6692ccec0466ff63e482972ef2d
pavel-k-petrov <you@example.com>
2021-02-18 23:58:04 +0300

    удалить tslint

--- ui/src/app/state/models/slalom-gates-state-model.ts
+++ ui/src/app/state/models/slalom-gates-state-model.ts
@@ -16,10 +16,10 @@ export interface SlalomGatesStateItemBase {
   attemptId: number;
   participantNumber: number;
 
-  // TODO: реализовать получение текущего пользователя при заполнении
-  // TODO: должно загружаться только для "ответственных" пользователей
+  // TODO реализовать получение текущего пользователя при заполнении
+  // TODO должно загружаться только для "ответственных" пользователей
   changeAuthor?: string;
-  // TODO: реализовать сервис времени
+  // TODO реализовать сервис времени
   changeTime?: number;
 }
 
